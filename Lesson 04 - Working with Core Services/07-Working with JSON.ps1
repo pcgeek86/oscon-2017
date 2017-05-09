516d937d94959c300761548ee3541a7e4cca7098
Trevor Sullivan <tsulli@amazon.com>
2017-05-09 07:50:32 -0700

    Add "working with JSON"

---
+++ Lesson 04 - Working with Core Services/07-Working with JSON.ps1
@@ -0,0 +1,44 @@
+# Working with JSON is easy in PowerShell
+# There are two commands you'll want to learn:
+#   - ConvertTo-Json
+#   - ConvertFrom-Json
+
+# Convert Process objects to JSON representation
+Get-Process | ConvertTo-Json
+
+# Look at the "Threads" property, and notice how it is referred to as a
+# System.Diagnostics.ProcessThread instance, instead of being expanded out.
+# The same behavior is occurring with the "Modules" property.
+
+# This is happening because the JSON is only being generated to a default depth of 2.
+
+# If we specify a higher depth for serialization, we get more detailed output.
+Get-Process | ConvertTo-Json -Depth 5
+
+# Convert simple JSON structure to a PowerShell object
+$PeopleString = @'
+{
+  "People": [
+    {
+      "FirstName": "Trevor",
+      "LastName": "Sullivan",
+    },
+    {
+      "FirstName": "Dave",
+      "LastName": "Sullivan",
+    },
+    {
+      "FirstName": "Joe",
+      "LastName": "Sullivan",
+    }
+  ]
+}
+'@
+
+$People = $PeopleString | ConvertFrom-Json
+
+# Look at the root object
+$People
+
+# Get the first person in the JSON array
+$People.People[0]
