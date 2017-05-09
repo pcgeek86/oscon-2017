bc13ebe519c7459d2df212291686021ca26f25b8
Trevor Sullivan <tsulli@amazon.com>
2017-05-09 08:06:13 -0700

    Add stub for PowerShell help

---
+++ Lesson 06 - Author a PowerShell Script Module/03-Authoring PowerShell Help.ps1
@@ -0,0 +1,9 @@
+# Authoring documentation for binary PowerShell modules is done using a XML format
+# called MAML. We don't have a complete example for this module, but be aware that it exists.
+
+# There's a simple way to author PowerShell help though, using Markdown. Microsoft 
+# created a PowerShell module called PlatyPS that translates Markdown into MAML help.
+
+# Install the PlatyPS module.
+Install-Module -Name PlatyPS -Scope CurrentUser -Force
+
