9b4ad8272f49d983b25807f1735a36e3a449f89c
Trevor Sullivan <tsulli@amazon.com>
2017-05-09 07:47:55 -0700

    Update class example

--- Lesson 09 - Working with .NET and Objects/04-PowerShell Classes/Family.ps1
+++ Lesson 09 - Working with .NET and Objects/04-PowerShell Classes/Family.ps1
@@ -1,5 +1,7 @@
 . $PSScriptRoot/Person.ps1
 
+# If you hover over the [Person[]] property definition below, you'll see it complain
+# that it can't find the Person class, even though we're dot-sourcing the file.
 class Family { 
   [Person[]] $Members
 }
