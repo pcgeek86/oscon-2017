1711866777f7bd9bfae08f7095fcf5396516f141
Trevor Sullivan <tsulli@amazon.com>
2017-05-09 08:07:26 -0700

    Add self-help resources

---
+++ Lesson 10 - Self-Help Resources/01-Self-Help Resources.ps1
@@ -0,0 +1,14 @@
+# Visit powershell.org. Use the discussion forums, read the blog posts.
+
+# Use Google. Duh. :)
+
+# Post a question on StackOverflow, under the "PowerShell" tag.
+
+# Use the #PowerShell hashtag on Twitter.
+# Use the #PSDSC hashtag on Twitter for DSC-related questions.
+
+# Follow me on ...
+#   Twitter: https://twitter.com/pcgeek86
+#    GitHub: https://github.com/pcgeek86
+#   Website: https://trevorsullivan.net
+#   YouTube: https://youtube.com/pcgeek86 
