601a8b09b4d57533a01546b64897d2bd2eb0429a
Trevor Sullivan <tsulli@amazon.com>
2017-05-09 07:20:01 -0700

    Adding "What we'll cover"

---
+++ Lesson 00 - Explore the PowerShell Environment/01-What We'll Cover.ps1
@@ -0,0 +1,16 @@
+Clear-Host
+
+Write-Host -Object 'We have 3.5 hours to cover a lot of content.'
+Write-Host -Object 'We''ll be covering lots of PowerShell fundamentals.'
+
+Read-Host
+
+Write-Host -Object @'
+- Setting up Visual Studio Code (VSCode) for PowerShell development
+- Understanding the PowerShell environment
+- Working with PowerShell modules
+- Calling and examining PowerShell commands
+- Reflecting over (inspecting) PowerShell objects
+- Working with .NET Core from PowerShell
+- Registering for object events
+'@
